fab1cb5cfa0816edc4e980375a0458e573b4215a
jrotolo <jrotolo91@gmail.com>
2014-04-30 19:51:40 -0500

    Finished spacing of bin string

--- cachesim.c
+++ cachesim.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int addr_space = 24;
 int block_size;
@@ -25,19 +26,15 @@ void GetFlags(int argc, char *argv[])
 		{
 			case 'b':
 				block_size = atoi(&argv[1][3]);
-				printf("Block size = %d\n", block_size);
 				break;
 			case 's':
 				num_sets = atoi(&argv[1][3]);
-				printf("Number of sets = %d\n", num_sets);
 				break;
 			case 'a':
 				assoc = atoi(&argv[1][3]);
-				printf("Associativity = %d\n", assoc);
 				break;
 			case 'W':
 				write_policy = argv[1][3];
-				printf("Write policy = %c\n", write_policy);
 				break;
 			default:
 				printf("Wrong Argument: %s\n", argv[1]);
@@ -51,25 +48,25 @@ void GetFlags(int argc, char *argv[])
 /* Prints header information for cache */
 void PrintHeader()
 {
-	int offset = num_sets;
+	int offset = block_size;
 	int index = num_sets;
-	int counter = 1;
+	int counter = 0;
 
-	while (offset != 0)
+	/* Log base 2 of block_size to find offset */
+	while (offset >>= 1)
 	{
-		++counter;
-		offset = offset >> 2;
+		counter++;
 	}
-	offset = counter++;
-	counter = 1;
+	offset = counter;
+	counter = 0;
+
+	/* Log base 2 of num_sets to find index */	
+	while (index >>= 1)
+	{
+		counter++;
+	}
+	index = counter;
 	
-	while (index != 0)
-	{
-		++counter;
-		index = index >> 2;
-	}
-	index = ++counter;
-
 	int tag = addr_space - offset - index;
 
 	cache_size = (num_sets * block_size * assoc) / 1024;
@@ -81,13 +78,13 @@ void PrintHeader()
 	printf("%dKB %d-way associative cache:\n", cache_size, assoc);
 	printf("%2s Block size = %d bytes\n", "",  block_size);
 	printf("%2s Number of [sets,blocks] = [%d,%d]\n", "", num_sets, num_blocks);
-	printf("%2s Extra space for tag storage = %d bytes (%1.3g%)\n", "", extra_space, percentage);
-	printf("%2s Bits for [tag,index,offset] = [%d,%d,%d] = %d\n", "", tag, index, offset, tag+index+offset);
+	printf("%2s Extra space for tag storage = %d bytes (%1.4g%)\n", "", extra_space, percentage);
+	printf("%2s Bits for [tag,index,offset] = [%d, %d, %d] = %d\n", "", tag, index, offset, tag+index+offset);
 	
-	if (write_policy == 't')
-		printf("%2s Write policy = %s\n", "", "Write-through");
-	else
+	if (write_policy == 'b')
 		printf("%2s Write policy = %s\n", "", "Write-back");
+	else
+		printf("%2s Write policy = %s\n", "", "Write-through");
 
 	printf("\n");
 	printf("%-7s %-36s %-5s %s %19s\n", "Hex", "Binary Address", "Set", "Blk", 
@@ -101,7 +98,7 @@ void PrintHeader()
 	
 }
 
-void HexToBin(char hex[], int hexc, char *bin[6])
+void HexToBin(char hex[], int hexc, char *bin[])
 {
 	int i = 0;
 	while (i != hexc)
@@ -109,52 +106,52 @@ void HexToBin(char hex[], int hexc, char *bin[6])
 		switch (hex[i])
 		{
 			case '0':
-				bin[i] = "0000\0";
+				bin[i] = "0000";
 				break;
 			case '1':
-				bin[i] = "0001\0";
+				bin[i] = "0001";
 				break;
 			case '2':
-				bin[i] = "0010\0";
+				bin[i] = "0010";
 				break;
 			case '3':
-				bin[i] = "0011\0";
+				bin[i] = "0011";
 				break;
 			case '4':
-				bin[i] = "0100\0";
+				bin[i] = "0100";
 				break;
 			case '5':
-				bin[i] = "0101\0";
+				bin[i] = "0101";
 				break;
 			case '6':
-				bin[i] = "0110\0";
+				bin[i] = "0110";
 				break;
 			case '7':
-				bin[i] = "0111\0";
+				bin[i] = "0111";
 				break;
 			case '8':
-				bin[i] = "1000\0";
+				bin[i] = "1000";
 				break;
 			case '9':
-				bin[i] = "1001\0";
+				bin[i] = "1001";
 				break;
 			case 'a':
-				bin[i] = "1010\0";
+				bin[i] = "1010";
 				break;
 			case 'b':
-				bin[i] = "1011\0";
+				bin[i] = "1011";
 				break;
 			case 'c':
-				bin[i] = "1100\0";
+				bin[i] = "1100";
 				break;
 			case 'd':
-				bin[i] = "1101\0";
+				bin[i] = "1101";
 				break;
 			case 'e':
-				bin[i] = "1110\0";
+				bin[i] = "1110";
 				break;
 			case 'f':
-				bin[i] = "1111\0";
+				bin[i] = "1111";
 				break;
 			default:
 				break;
@@ -187,9 +184,24 @@ int main(int argc, char *argv[])
 	HexToBin(hex, hexc, bin);
 
 	int i = 1;
-	printf("%6s", bin[0]);
-	for (i; i < hexc; i++)
-		printf("%s", bin[i]);	
+   printf("%7s", bin[0]);
+	//for (i; i < hexc; i++)
+	//	printf("%s", bin[i]);	
+	//printf("\n");
+
+	char *str = bin[0];
+	char *newstr;
+	newstr = malloc(strlen(str)+1+4);
+	strcpy(newstr, str);
+	for (i=1; i < hexc; i++)
+		strcat(newstr, bin[i]);
+
+	for (i = 0; i < addr_space; i++) {
+		printf("%c", newstr[i]);
+		//printf("i=%d\n", i);
+		if (i == 8 || i == 14 )
+				  printf(" ");
+	}
 	printf("\n");
 	return 0;
 }
